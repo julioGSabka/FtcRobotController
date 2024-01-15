56ae86502e4c3c61b4235ce5f5498edbd9b455b4
julioGSabka <juliogustavosabka@gmail.com>
2024-01-15 20:49:13 -0300

    DriveConstants 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerScripts/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerScripts/drive/DriveConstants.java
@@ -33,7 +33,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 47);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(22, 0, 10, 13);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -44,7 +44,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.47; // in
-    public static double GEAR_RATIO = 1.052910; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 15; // in
 
     /*
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.03177;
-    public static double kA = 0.00013;
-    public static double kStatic = 0.39797;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0;
+    public static double kStatic = 0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
