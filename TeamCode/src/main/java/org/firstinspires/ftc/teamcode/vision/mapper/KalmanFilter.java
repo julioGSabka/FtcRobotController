df279f1667584b92a2971c1eb20bfc07fe50a494
felipetrentin <felipetrentintecnico@hotmail.com>
2024-01-27 16:08:54 -0300

    kalma separado

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/mapper/KalmanFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/mapper/KalmanFilter.java
@@ -7,45 +7,41 @@ import org.apache.commons.math3.linear.RealMatrix;
 import java.util.List;
 
 public class KalmanFilter {
-    RealMatrix x; // your initial state
+    double x; // your initial state
     double Q = 0.1; // your model covariance
     double R = 0.4; // your sensor covariance
-    RealMatrix p; // your initial covariance guess
-    RealMatrix K; // your initial Kalman gain guess
+    double p; // your initial covariance guess
+    double K; // your initial Kalman gain guess
 
-    RealMatrix x_previous = x;
-    RealMatrix p_previous = p;
-    RealMatrix u;
-    RealMatrix z;
+    double x_previous = x;
+    double p_previous = p;
+    double u;
+    double z;
 
     KalmanFilter(double Q, double R){
         this.Q = Q;
         this.R = R;
     }
 
-    Pose2d updateFilter(Pose2d velocity, List<Pose2d> tagEstimates){
+    double updateFilter(double velocity, double pose){
         //TODO: converter pose2d para real matrix
 
-        u = getInput(); // Ex: change in position from odometry.
+        u = velocity; // Ex: change in position from odometry.
+        x = x_previous + u;
 
+        p = p_previous + Q;
 
-        x = x_previous.add(u);
+        K = p/(p + R);
 
-        p = p_previous.scalarAdd(Q);
+        z = pose; // Pose Estimate from April Tag / Distance Sensor
 
-        //TODO: fazer divisão para cada elemento de la matrizzz
-        K = p/(p.scalarAdd(R));
+        x = x + K * (z - x);
 
-        //TODO: converter pose2d para real matrix
-        z = getSecondSensor(); // Pose Estimate from April Tag / Distance Sensor
-
-        x = x.add(K.multiply(z.subtract(x)));
-
-        p = (K.scalarMultiply(-1).scalarAdd(1)).multiply(p);
+        p = (1 - K) * p;
 
         x_previous = x;
         p_previous = p;
 
-        return new Pose2d();
+        return x;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/mapper/kalmannPose.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.vision.mapper;
+
+import com.arcrobotics.ftclib.geometry.Pose2d;
+
+import java.util.List;
+
+public class kalmannPose {
+    KalmanFilter xFilter;
+    KalmanFilter yFilter;
+    KalmanFilter wFilter;
+
+    Pose2d updateFilter(Pose2d vel, List<Pose2d> poses){
+
+    }
+
+}
