355992073c34d4085c68d55756b1c71f59ae2dec
julioGSabka <juliogustavosabka@gmail.com>
2024-02-05 11:45:36 -0300

    Field Oriented Drive Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldOrientedDriveTest.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@TeleOp
+public class FieldOrientedDriveTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx motorFrontLeft = null;
+    private DcMotorEx motorBackLeft = null;
+    private DcMotorEx motorFrontRight = null;
+    private DcMotorEx motorBackRight = null;
+
+    IMU imu;
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft"); //0
+        motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft"); //1
+        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight"); //2
+        motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight"); //3
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        //Configure Motors
+        motorBackRight.setDirection(DcMotorEx.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorEx.Direction.REVERSE);
+
+        waitForStart();
+        resetRuntime();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+
+            double velocity = (gamepad1.right_trigger * 0.70) + 0.20;
+            double y = -gamepad1.left_stick_y * velocity;
+            double x = gamepad1.left_stick_x * velocity;
+            double rx = gamepad1.right_stick_x * velocity;
+
+            if (gamepad1.start) {
+                imu.resetYaw();
+            }
+
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+
+            double gyro = orientation.getYaw(AngleUnit.RADIANS);
+            double rotX = x * Math.cos(-gyro) - y * Math.sin(-gyro);
+            double rotY = x * Math.sin(-gyro) + y * Math.cos(-gyro);
+
+            rotX = rotX * 1.1;
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+
+        }
+    }
+}
